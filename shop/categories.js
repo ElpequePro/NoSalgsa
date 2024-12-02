532953fe290b45fcb5e232eeb55a87c8c782f3dc
akinfenwa69 <popo418@vidalibarraquer.net>
2024-12-02 09:14:56 +0100

    Update categories.js

--- shop/categories.js
+++ shop/categories.js
@@ -42,26 +42,3 @@ function categories(name) {
         document.getElementById('subscripcions').classList.add('active');
     }
 }
-
-// SEARCH
-
-search = document.getElementById('search_bar');
-
-function btnSearch() {
-    div = document.getElementById('search_div');
-    div.classList.toggle('active');
-    if (div.classList.contains('active')) {
-        search.focus();
-    }
-}
-
-search.addEventListener('keydown', () => {
-    localStorage.setItem('query', search.value);
-})
-
-search.addEventListener('keypress', function (e) {
-    if (e.key == 'Enter') {
-        localStorage.setItem('load', 'true');
-        window.location.replace('/shop/productes.html');
-    }
-})
