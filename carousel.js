6e5fd790cc724ec7d41270d9493f78ed33db9687
Pol Poblet Pallisé <popo418@vidalibarraquer.net>
2025-02-28 10:20:18 +0100

    change carousel

--- carousel.js
+++ carousel.js
@@ -1,26 +1,51 @@
 car = document.getElementById('carousel');
 
 bgcolor = "linear-gradient(0deg, rgba(17, 17, 17, .7) 0%, rgba(51, 51, 51, .5) 100%)";
-url = ["img/carousel1.png", "img/carousel2.png", "img/carousel3.png"];
+list = ["img/carousel1.png", "img/carousel2.png", "img/carousel3.png", "img/carousel4.png"];
 
 function carousel() {
-    changeCarousel();
-}
+    car.style.background = `${bgcolor}, url("${list[0]}")`;
+    car.style.backgroundSize = 'cover';
+    car.style.backgroundPosition = 'center center';}
 
-i = 0;
+var i = 0;
 
-function changeCarousel() {
-    // background: [background-color] [background-image] [background-repeat] [background-position] [background-size] [background-attachment] [background-clip] [background-origin] [background-composite];
-    car.style.background = bgcolor + ", url('" + url[i] + "')";
-    car.style.backgroundSize = "cover";
-    car.style.backgroundPosition = "center center";
+function changeCarousel(name) {
+    if (name == 'left') {
+        if (i == 0) {
+            i = list.length - 1;
+        } else {
+            i--;
+        }
 
+        car.style.background = `${bgcolor}, url("${list[i]}")`;
+        car.style.backgroundSize = 'cover';
+        car.style.backgroundPosition = 'center center';
 
-    i++;
+        clearInterval(timer);
+        timer = setInterval(function () {
+            changeCarousel('right');
+        }, 5000);
 
-    if (i >= url.length) {
-        i = 0;
+    } else if (name == 'right') {
+        if (i == list.length - 1) {
+            i = 0;
+        } else {
+            i++;
+        }
+
+        car.style.background = `${bgcolor}, url("${list[i]}")`;
+        car.style.backgroundSize = 'cover';
+        car.style.backgroundPosition = 'center center';
+
+        clearInterval(timer);
+        timer = setInterval(function () {
+            changeCarousel('right');
+        }, 5000);
+
     }
 }
 
-setInterval(changeCarousel, 5000);
+setInterval(function () {
+    changeCarousel('right');
+}, 5000);
